98af9aca24d032290b13ebc7c4df9cec7874a5c5
Sagar Patel <sagarp@coyote.csusb.edu>
2018-10-27 23:32:48 -0700

    Added Player Movement except Jump

--- MarshMallowPoppers/Assets/Scripts/Player_Controller.cs
+++ MarshMallowPoppers/Assets/Scripts/Player_Controller.cs
@@ -12,28 +12,58 @@ public class Player_Controller : MonoBehaviour {
     private KeyCode pause;
     private Rigidbody2D rigidBody;
     private bool isGrounded;
+    [SerializeField] [Range(0, 50)] private float speed = 0;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         rigidBody = GetComponent<Rigidbody2D>();
         moveRight = KeyCode.D;
         moveLeft  = KeyCode.A;
         jump      = KeyCode.Space;
         fire      = KeyCode.Mouse0;
+        isGrounded = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Movement();
         Attack();
-        Jump();
+        //Jump();
 	}
 
     void Movement() {
 
+        if (Input.GetKey(moveRight))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(moveLeft)) {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(jump) && isGrounded == true)
+        {
+            Debug.Log("SpaceBar is pressed", gameObject);
+            Jump();
+        }
     }
 
-    void Attack() { }
+    void Attack() {
 
-    void Jump() { }
+    }
+
+    void Jump() {
+        Debug.Log("Jump button is pressed", gameObject);
+        isGrounded = false;
+        rigidBody.AddForce(Vector2.up * 250.0f);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
 }
