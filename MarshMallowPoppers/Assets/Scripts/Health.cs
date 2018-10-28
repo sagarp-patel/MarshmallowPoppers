440d90a24fbfe781b5d07cc20a8e1fcef0c30e88
Sagar Patel <sagarp@coyote.csusb.edu>
2018-10-27 22:23:50 -0700

    Pushed Scripts and other Assets

---
+++ MarshMallowPoppers/Assets/Scripts/Health.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public int hitPoints;
+    private int maxHitPoints;
+
+	// Use this for initialization
+	void Start () {
+        maxHitPoints = hitPoints;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ MarshMallowPoppers/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+
+    private KeyCode moveRight;
+    private KeyCode moveLeft;
+    private KeyCode jump;
+    private KeyCode changeWeapon;
+    private KeyCode fire;
+    private KeyCode pause;
+    private Rigidbody2D rigidBody;
+    private bool isGrounded;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+        moveRight = KeyCode.D;
+        moveLeft  = KeyCode.A;
+        jump      = KeyCode.Space;
+        fire      = KeyCode.Mouse0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+        Attack();
+        Jump();
+	}
+
+    void Movement() {
+
+    }
+
+    void Attack() { }
+
+    void Jump() { }
+}
