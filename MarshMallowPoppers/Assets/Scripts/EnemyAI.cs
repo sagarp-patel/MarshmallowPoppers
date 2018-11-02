09e6eaf2b43a96f912e2785d63909bed22c3bc8c
Sagar Patel <sagarp@coyote.csusb.edu>
2018-11-01 17:54:30 -0700

    Enemy AI Script Added

---
+++ MarshMallowPoppers/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    private Transform player;
+    public GameObject projectile;
+
+    private float timeBShots;
+    public float startTimeBShots;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        timeBShots = startTimeBShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+        if (timeBShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBShots = startTimeBShots;
+        }
+        else
+        {
+            timeBShots -= Time.deltaTime;
+        }
+    }
+}
