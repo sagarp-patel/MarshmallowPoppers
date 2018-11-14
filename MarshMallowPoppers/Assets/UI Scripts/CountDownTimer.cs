e2c4483f8987a767d53558eab3bc9dd74a3e45bd
Nicholas Perez <004362227@coyote.csusb.edu>
2018-11-13 19:26:34 -0800

    Added UI stuff, folders: UI scripts, Menu_music, Nick_Assets, Practice_Scenes

---
+++ MarshMallowPoppers/Assets/UI Scripts/CountDownTimer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountDownTimer : MonoBehaviour
+{
+    public int currentTime = 0;
+    public int startingTime = 10;
+    public Text countdownText;
+    public GameObject GameOver;
+    public GameObject DisableUI;
+
+    void Start()
+    {
+        InvokeRepeating("Count", 0.0f, 1.0f);
+        currentTime = startingTime;
+        GameOver.SetActive(false);
+        DisableUI.SetActive(true);
+    }
+    void Count()
+    {
+        if (currentTime == 0)
+        {
+            //currentTime = startingTime;
+            GameOver.SetActive(true);
+            DisableUI.SetActive(false);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            currentTime = currentTime - 1;
+        }
+        countdownText.text = currentTime.ToString();
+
+    }
+}
