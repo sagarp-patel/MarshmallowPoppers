a99d1e2e2a1660e0de36a4effbc3feb5ce3f6cb0
Chad <mcadamschad@outlook.com>
2018-11-13 19:02:43 -0800

    scores test

---
+++ MarshMallowPoppers/Assets/Scripts/Scores.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scores : MonoBehaviour {
+
+    //public GameObject funny;
+    public int score;
+
+    // Use this for initialization
+    void Start () {
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            score = score  + 5;
+            Debug.Log(score.ToString());
+        }
+
+     }
+}
