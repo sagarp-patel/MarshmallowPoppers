aeed9dc413868f48c9e449506cfa5f2b2a19cf2e
nmperez00 <43743036+nmperez00@users.noreply.github.com>
2018-11-19 02:55:58 -0800

    ...

--- MarshMallowPoppers/Assets/UI Scripts/ScoreSystem.cs
+++ MarshMallowPoppers/Assets/UI Scripts/ScoreSystem.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Reflection;
 
 public class ScoreSystem : MonoBehaviour
 {
-
-    public GameObject bullet;
-    public GameObject enemyobject;
+    public enum PlayerType { ENEMY, PLAYER };
+    public enum Type { CHOCOLATE, MARSHMALLOW, PEPPERMINT };
     public int score;
+    public PlayerType playerType;
 
     // Use this for initialization
     void Start()
@@ -23,12 +24,43 @@ public class ScoreSystem : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Projectile" && collision.gameObject.tag == "Player")
-         {
+        /* if (collision.gameObject.tag == "Enemy")
+          {
+             Health healthstatus = collision.gameObject.GetComponent<Health>();
+             //score = score  + 5;
+             Debug.Log("Hello!!!!!!!!!" + score.ToString());
+          }*/
+        if (collision.gameObject.tag == "Projectile")
+        {
             Projectile otherProjectile = collision.gameObject.GetComponent<Projectile>();
-            //score = score  + 5;
-            Debug.Log("Hello!!!!!!!!!" /*+ score.ToString()*/);
-         }
+            if (playerType == PlayerType.ENEMY && otherProjectile.playerType == Projectile.PlayerType.PLAYER)
+            {
+                //ClearLog();
+                //Debug.Log("HEllO!!!!!!");
+            }
+            if (playerType == PlayerType.PLAYER && otherProjectile.playerType == Projectile.PlayerType.PLAYER)
+            {
+
+            }
+            else
+            {
+               //ClearLog();
+            }
+
+            if (collision.gameObject.tag == "Player" && playerType == ScoreSystem.PlayerType.ENEMY)
+            {
+                //ClearLog();
+                Debug.Log("HEllO TO YOU TOO!!!!!!");
+            }
+        }
+    }
+
+    public void ClearLog()
+    {
+        var assembly = Assembly.GetAssembly(typeof(UnityEditor.Editor));
+        var type = assembly.GetType("UnityEditor.LogEntries");
+        var method = type.GetMethod("Clear");
+        method.Invoke(new object(), null);
     }
     /*
     public void IsDestroyed(enemyobject)
