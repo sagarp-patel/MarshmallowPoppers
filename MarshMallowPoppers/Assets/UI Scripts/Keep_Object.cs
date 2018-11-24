158b3835b65d7dd279b2e0d9981d9591814925a6
nmperez00 <43743036+nmperez00@users.noreply.github.com>
2018-11-23 22:38:09 -0800

    Temp changes

--- MarshMallowPoppers/Assets/UI Scripts/Keep_Object.cs
+++ MarshMallowPoppers/Assets/UI Scripts/Keep_Object.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Keep_Object : MonoBehaviour
 {
-	void Awake()
+    public Toggle ToggleButton;
+    /*void Awake()
     {
         GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
         if (objs.Length > 1)
@@ -12,5 +14,16 @@ public class Keep_Object : MonoBehaviour
             Destroy(this.gameObject);
         }
         DontDestroyOnLoad(this.gameObject);
+    }*/
+    void Awake()
+    {
+        bool togglesound = gameObject.GetComponent<Toggle>().isOn;
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Toggle");
+        if (objs.Length > 1 && togglesound)
+        {
+            Destroy(objs[0].gameObject);
+            ToggleButton.isOn = false;
+        }
+        DontDestroyOnLoad(objs[1].gameObject);
     }
 }
