214970dde14c1ceb36b8cf1073aea52e9a30c3f4
Sagar Patel <sagarp@coyote.csusb.edu>
2018-11-24 14:55:19 -0800

    Added Background Object

---
+++ MarshMallowPoppers/Assets/Scripts/Background_Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background_Controller : MonoBehaviour {
+
+    [SerializeField] [Range(0.0f, 10.0f)] private float sensitivity;
+	[SerializeField] [Range(0.0f, 10.0f)] private float smoothing;
+    public Transform Player;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector2(Player.position.x, Player.position.y);
+	}
+}
