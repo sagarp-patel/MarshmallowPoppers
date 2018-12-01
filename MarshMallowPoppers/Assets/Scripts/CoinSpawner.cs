2763ef2f4e517837e4eae5264826ead63a9a2706
Kajal <kajalrama16@gmail.com>
2018-11-30 22:10:03 -0800

    coin Spawn

---
+++ MarshMallowPoppers/Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] coins;
+    int randomSpawnPoint, randomCoins;
+    public static bool spawnAllowed;
+
+    // Use this for initialization
+    void Start()
+    {
+        spawnAllowed = true;
+        InvokeRepeating("SpawnAMonster", 0f, 3f);
+    }
+    void SpawnAMonster()
+    {
+        if (spawnAllowed)
+        {
+            randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+            randomCoins = Random.Range(0, coins.Length);
+           Instantiate(coins[randomCoins], spawnPoints[randomSpawnPoint].position,
+               Quaternion.identity);
+        }
+    }
+}
+
---
+++ MarshMallowPoppers/Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public GameObject[] coins;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public int startWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    //public bool stop;
+    int randcoin;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(coinSpawner());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+	}
+    IEnumerator coinSpawner()
+    {
+        yield return new WaitForSeconds (startWait);
+        while(true)
+        {
+            randcoin = Random.Range(0,1);
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), Random.Range(-spawnValues.y, spawnValues.y),1);
+            Instantiate(coins[randcoin], spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
